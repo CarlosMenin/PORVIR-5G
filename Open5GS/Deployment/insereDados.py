490afc68aa3c90e8e19275f95b4dd712a9ce50ed
CarlosMenin <92284071+CarlosMenin@users.noreply.github.com>
2023-10-03 15:21:57 -0300

    Add files via upload

---
+++ Open5GS/Deployment/insereDados.py
@@ -0,0 +1,136 @@
+import pymongo
+from bson import ObjectId
+from bson import Int64
+
+# Conexão com o servidor do MongoDB. Alterar a porta específica do serviço do MongoDB
+client = pymongo.MongoClient("mongodb://localhost:44789/")
+
+# Acessa a base de dados e seleciona a coleção
+db = client["open5gs"]
+collection = db["subscribers"]
+
+# Define uma lista de documentos a ser passada para a base de dados posteriormente
+documentos = []
+
+# Define o valor inicial para o imsi
+valor_inicial = "999700000000000"
+
+# Define a quantidade de UEs que queira cadastrar
+qtd_UE = 1000
+
+# Cria os documentos 
+for num_UE in range(qtd_UE):
+    documento = {
+    "_id" : ObjectId(),
+    "imsi" : str(int(valor_inicial) + num_UE),
+    "subscribed_rau_tau_timer" : int(12),
+    "network_access_mode" : int(0),
+    "subscriber_status" : int(0),
+    "access_restriction_data" : int(32),
+    "slice" : [
+        {
+            "sst" : int(1),
+            "default_indicator" : False,
+            "_id" : ObjectId(),
+            "session" : [
+                {
+                    "name" : "internet",
+                    "type" : int(3),
+                    "_id" : ObjectId(),
+                    "pcc_rule" : [
+
+                    ],
+                    "ambr" : {
+                        "uplink" : {
+                            "value" : int(1),
+                            "unit" : int(3)
+                        },
+                        "downlink" : {
+                            "value" : int(1),
+                            "unit" : int(3)
+                        }
+                    },
+                    "qos" : {
+                        "index" : int(9),
+                        "arp" : {
+                            "priority_level" : int(8),
+                            "pre_emption_capability" : int(1),
+                            "pre_emption_vulnerability" : int(1)
+                        }
+                    }
+                }
+            ]
+        },
+        {
+            "sst" : int(1),
+            "sd" : "000001",
+            "_id" : ObjectId(),
+            "default_indicator" : True,
+            "session" : [
+                {
+                    "name" : "slice01",
+                    "type" : int(3),
+                    "_id" : ObjectId(),
+                    "pcc_rule" : [
+
+                    ],
+                    "ambr" : {
+                        "uplink" : {
+                            "value" : int(1),
+                            "unit" : int(3)
+                        },
+                        "downlink" : {
+                            "value" : int(1),
+                            "unit" : int(3)
+                        }
+                    },
+                    "qos" : {
+                        "index" : int(5),
+                        "arp" : {
+                            "priority_level" : int(1),
+                            "pre_emption_capability" : int(1),
+                            "pre_emption_vulnerability" : int(1)
+                        }
+                    }
+                }
+            ]
+        }
+    ],
+    "ambr" : {
+        "uplink" : {
+            "value" : int(1),
+            "unit" : int(3)
+        },
+        "downlink" : {
+            "value" : int(1),
+            "unit" : int(3)
+        }
+    },
+    "security" : {
+        "k" : "465B5CE8B199B49FAA5F0A2EE238A6BC",
+        "amf" : "8000",
+        "op" : None,
+        "opc" : "E8ED289DEBA952E4283B54E88E6183CA",
+        "sqn" : Int64()
+    },
+    "purge_flag" : [
+
+    ],
+    "mme_realm" : [
+
+    ],
+    "mme_host" : [
+
+    ],
+    "imeisv" : "1110000000000000",
+    "msisdn" : [
+
+    ],
+    "schema_version" : int(1),
+    "__v" : int(0),
+    "inUse" : int(1)
+}
+    documentos.append(documento)
+
+# Insere todos os documentos na coleção
+resultado = collection.insert_many(documentos)
