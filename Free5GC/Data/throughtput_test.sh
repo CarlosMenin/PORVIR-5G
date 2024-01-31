f361d1c82096ff7595af9b896ffc902b78eacc38
CarlosMenin <92284071+CarlosMenin@users.noreply.github.com>
2024-01-31 15:10:29 -0300

    Add files via upload

--- Free5GC/Data/throughtput_test.sh
+++ Free5GC/Data/throughtput_test.sh
@@ -1,10 +1,10 @@
-cd Deployment
-python3 start.py
+#cd Deployment
+#python3 start.py
 
-kubectl port-forward deployment/free5gc-mongodb 63145:27017 --namespace free5gc &
-PORT_FORWARD_PID=$!
+#kubectl port-forward deployment/free5gc-mongodb 63145:27017 --namespace free5gc &
+#PORT_FORWARD_PID=$!
 
-cd Database
+cd Deployment/Database
 
 py_names=("policyData.ues.amData.py" "policyData.ues.qosFlow" "policyData.ues.smData" "subscriptionData.authenticationData.authenticationSubscription" "subscriptionData.provisionedData.amData" "subscriptionData.provisionedData.smData" "subscriptionData.provisionedData.smfSelectionSubscriptionData")
 
@@ -12,7 +12,7 @@ for arq_name in "${py_names[@]}"; do
     python3 "$arq_name"
 done
 
-kill $PORT_FORWARD_PID
+#kill $PORT_FORWARD_PID
 
 cd ../../Data
 
@@ -29,7 +29,7 @@ for i in 1 2 4 6 8 10; do
     echo "Starting experiment $i"
     for j in $(seq 0 $(($i - 1))); do
         IP=$(kubectl exec -i -n free5gc free5gc-my5grantester-$j -c my5grantester -- sh -c "ip -4 addr show uetun1 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'")
-        kubectl exec -i -n free5gc free5gc-my5grantester-$j -c my5grantester -- sh -c "iperf -c iperf --bind $IP -p 5001 -t 60 -i 1 -y C" > my5grantester-iperf-1-$i-$j.csv &
+        kubectl exec -i -n free5gc free5gc-my5grantester-$j -c my5grantester -- sh -c "iperf -c iperf --bind $IP -p 5001 -t 60 -i 1 -y C" > my5grantester-iperf-1-1-$i-$j.csv &
     done
 
     echo "Waiting for experiment $i to finish"
